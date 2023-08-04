2e0288229091c4b4bfe6a2a403da66000810ee00
MariusAskelovic <72157256+MariusAskelovic@users.noreply.github.com>
2023-08-04 14:11:22 +0300

    hero html and css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './style/common.css';
 import './style/App.css';
 import Header from './components/layout/Header';
+import Hero from './components/layout/Hero';
 
 function App() {
   return (
     <div className='App container'>
       <Header />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/layout/Hero.jsx
@@ -0,0 +1,9 @@
+function Hero() {
+  return (
+    <div className='heroBg'>
+      <img className='heroImg' src='' alt='Header Image' />
+    </div>
+  );
+}
+
+export default Hero;
